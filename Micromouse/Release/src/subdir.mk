326cca5b0213798fd9393e4bb7501de858f7316f
UCSD-MM <ieeemicromouse2015@gmail.com>
2016-04-15 23:52:01 -0700

    Hello git

---
+++ Micromouse/Release/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/_write.c \
+../src/main.c \
+../src/stm32f4xx_hal_msp.c \
+../src/stm32f4xx_it.c 
+
+OBJS += \
+./src/_write.o \
+./src/main.o \
+./src/stm32f4xx_hal_msp.o \
+./src/stm32f4xx_it.o 
+
+C_DEPS += \
+./src/_write.d \
+./src/main.d \
+./src/stm32f4xx_hal_msp.d \
+./src/stm32f4xx_it.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra  -g -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F405xx -DUSE_HAL_DRIVER -DHSE_VALUE=25000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ test2_0229/Release/src/subdir.mk
@@ -0,0 +1,59 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/BlinkLed.c \
+../src/Timer.c \
+../src/_initialize_hardware.c \
+../src/_write.c \
+../src/main\ 2.c \
+../src/main.c \
+../src/stm32f4xx_hal_msp.c \
+../src/stm32f4xx_it.c 
+
+OBJS += \
+./src/BlinkLed.o \
+./src/Timer.o \
+./src/_initialize_hardware.o \
+./src/_write.o \
+./src/main\ 2.o \
+./src/main.o \
+./src/stm32f4xx_hal_msp.o \
+./src/stm32f4xx_it.o 
+
+C_DEPS += \
+./src/BlinkLed.d \
+./src/Timer.d \
+./src/_initialize_hardware.d \
+./src/_write.d \
+./src/main\ 2.d \
+./src/main.d \
+./src/stm32f4xx_hal_msp.d \
+./src/stm32f4xx_it.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra  -g -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F405xx -DUSE_HAL_DRIVER -DHSE_VALUE=25000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/main\ 2.o: ../src/main\ 2.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra  -g -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F405xx -DUSE_HAL_DRIVER -DHSE_VALUE=25000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"src/main 2.d" -MT"src/main\ 2.d" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/stm32f4xx_hal_msp.o: ../src/stm32f4xx_hal_msp.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra  -g -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F405xx -DUSE_HAL_DRIVER -DHSE_VALUE=25000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -Wno-missing-prototypes -Wno-missing-declarations -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/stm32f4xx_hal_msp.d" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
